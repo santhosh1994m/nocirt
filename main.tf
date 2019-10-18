57bbd9b0c270a78ed4cf51a0d8d7443561de38f9
santhosh1994m <45985363+santhosh1994m@users.noreply.github.com>
2019-10-19 00:14:38 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "google" {
+    credentials = "${{GOOGLE_APPLICATION_CREDENTIALS}}"
+    project     = "${{GCLOUD_PROJECT_ID}}"
+    region      = "us-west1"
+}
+
+resource "google_storage_bucket" "image-store" {
+  name     = "image-store-bucket-santhosh"
+  location = "EU"
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+} 
