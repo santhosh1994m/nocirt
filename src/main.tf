a30ac327d84b04f7b53df965d33944c4124cbb44
santhosh1994m <45985363+santhosh1994m@users.noreply.github.com>
2019-10-19 17:41:50 +0530

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -1,8 +1,7 @@
 provider "google" {
    region      = "us-west1"
-  
+   credentials = "${file("logical-voyage-252303-6ab62b7de59c.json")}"
    }
 resource "google_storage_bucket" "image-store" {
   name     = "image-store-bucket-santhosh123"
-  location = "EU"
    }
