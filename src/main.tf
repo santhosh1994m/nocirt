84ce3a25fb30dcfbadf33c8204d5fd124b59d26d
santhosh1994m <45985363+santhosh1994m@users.noreply.github.com>
2019-10-19 17:09:22 +0530

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -1,6 +1,6 @@
 provider "google" {
    region      = "us-west1"
-   version     = "~> 3.0.0"
+  
    }
 resource "google_storage_bucket" "image-store" {
   name     = "image-store-bucket-santhosh12"
