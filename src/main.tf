bfe5685dc99ec4a0f11fa8d297547a41fe2990d4
santhosh1994m <45985363+santhosh1994m@users.noreply.github.com>
2019-10-19 17:54:26 +0530

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -1,4 +1,12 @@
 provider "google" {
-   region      = "us-west1"
       }
 
+resource "google_storage_bucket" "image-store" {
+  name     = "image-store-bucket-santhoshkumarm2"
+  location = "EU"
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
