28be1469fd8e1ad630dcfe1301be89a6e6d5122b
santhosh1994m <45985363+santhosh1994m@users.noreply.github.com>
2019-10-19 13:36:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-    credentials = "${{GOOGLE_APPLICATION_CREDENTIALS}}"
+    
     project     = "${{GCLOUD_PROJECT_ID}}"
     region      = "us-west1"
     version = "~> 3.0.0"
